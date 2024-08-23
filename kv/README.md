f40d3bd7ebc67fdbc0183ffa42838b2ebcfedb3b
Jessop Breth <jessopb@gmail.com>
2024-08-23 19:17:57 -0400

    move batch to extension

---
+++ kv/README.md
@@ -0,0 +1,15 @@
+# Base
+Base functions of the KV
+- get()
+- set()
+- getPrefix()
+- del()
+- start_batch()
+
+# Extensions
+- "Normalized" keys
+- "Lists"
+  - sorted
+- "Encoded" values
+  - encoding binary data for value
+  - encoding json for value
--- kv/base/src/kv.lua
+++ kv/base/src/kv.lua
@@ -70,37 +70,6 @@ function KV:write(batch)
     batch:destroy()
 end
 
-function KV:start_batch()
-    return Batch.new()
-end
-
-function Batch:set(keyString, value)
-    table.insert(self.operations, { op = "set", key = keyString, value = value })
-end
-
-function Batch:delete(keyString)
-    table.insert(self.operations, { op = "delete", key = keyString })
-end
-
-function Batch:execute(kv)
-    for _, operation in ipairs(self.operations) do
-        if operation.op == "set" then
-            kv:set(operation.key, operation.value)
-        elseif operation.op == "delete" then
-            kv:del(operation.key)
-        end
-    end
-end
-
-function Batch:clear()
-    self.operations = {}
-end
-
-function Batch:destroy()
-    self:clear()
-    setmetatable(self, nil)
-end
-
 return KV
 
 
