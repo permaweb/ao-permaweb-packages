018e600ef7c04253c300d744ece65359f423de58
Jessop Breth <jessopb@gmail.com>
2024-09-09 17:14:10 -0400

    kv/batch main.lua

--- kv/batchplugin/dist/main.lua
+++ kv/batchplugin/dist/main.lua
@@ -17,18 +17,13 @@ function BatchPlugin.new()
         function batch:set(keyString, value)
             table.insert(self.operations, { op = "set", key = keyString, value = value })
         end
-
-        --function batch:set(keyString, value)
-        --    table.insert(self.operations, { op = "set", key = keyString, value = value })
-        --end
+        -- TODO probably implement del?
 
         -- Execute all batched operations
         function batch:execute()
             for _, operation in ipairs(self.operations) do
                 if operation.op == "set" then
                     kv:set(operation.key, operation.value)
-                --elseif operation.op == "del" then
-                --    kv:del(operation.key)
                 end
             end
             self:clear()  -- Optionally clear the batch after execution
